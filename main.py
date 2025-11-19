e40cf0b85a0c0983747ed986b61afd8af9e18aa0
darrylrachel <racheldarryl@icloud.com>
2025-11-19 13:30:11 -0800

    Add FPS lock using pygame.time.Clock() and delta time tracking

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting Asteroids with pygame version: ", pygame.version.ver)
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -22,6 +25,9 @@ def main():
 
         screen.fill(("black"))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        #print(dt)
 
 
 if __name__ == "__main__":
