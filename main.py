08cf62badfe202d8052877a6b0c3cbb41f0d24eb
darrylrachel <racheldarryl@icloud.com>
2025-11-20 11:52:38 -0800

    Fix: rename updateable → updatable to satisfy test expectations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 
@@ -11,19 +12,36 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    # Create groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # Tells Player which group to auto-add to
+    Player.containers = (updatable, drawable)
+
+    # Creates the player and will register to itseld to the groups
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     print("Starting Asteroids with pygame version: ", pygame.version.ver)
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    
     while True:
         log_state()
-
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+                
+        # player.update(dt) # update player state first
+        updatable.update(dt)
 
         screen.fill(("black"))
+
+        # Draw everything in drawable
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
@@ -32,3 +50,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    pygame.quit()
